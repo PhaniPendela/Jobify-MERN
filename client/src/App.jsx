0c300a75b0e03803d1ffe3ae1ab7193979843e80
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-07-03 17:06:27 +0530

    Started Initial Routing and Implemented small nested routing, created Pages for the project with basic boilerplate.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  HomeLayout,
+  Landing,
+  Register,
+  Login,
+  DashboardLayout,
+  Error,
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout />,
+      },
+      {
+        index: true,
+        element: <Landing />,
+      },
+    ],
+  },
+]);
 
 const App = () => {
-  return <h1>Jobify</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
