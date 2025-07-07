465a2c2ab66728edb439198ac1f277b0d9a75e08
Pendela V.P.B Krishna Chaitanya <67474140+PhaniPendela@users.noreply.github.com>
2025-07-07 15:14:47 +0530

    Finished Register User

--- controller/authController.js
+++ controller/authController.js
@@ -1,11 +1,13 @@
 import { StatusCodes } from "http-status-codes";
 import userModel from "../models/userModel.js";
+import { hashPassword } from "../utils/passwordUtils.js";
 
 export const register = async (req, res) => {
   const isFirstUser = (await userModel.countDocuments()) === 0;
   req.body.role = isFirstUser ? "admin" : "user";
-  const user = await userModel.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  req.body.password = await hashPassword(req.body.password);
+  await userModel.create(req.body);
+  res.status(StatusCodes.CREATED).json({ message: "User Created" });
 };
 export const login = async (req, res) => {
   res.send("login");
